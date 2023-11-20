acf216cd799b9503584fba040e4bed81a1950463
Thiên Nguyễn <broagii@yahoo.com>
2023-11-20 07:27:20 +0700

    fix metadata settings

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <link rel=icon href=images/favicon.ico type=image/x-icon>
     <link rel="shortcut icon" href=images/favicon.ico type=image/x-icon>
@@ -9,7 +10,7 @@
 
     <link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="/font-awesome/css/all.css">
-    <link rel="stylesheet" type="text/css" href="/css/slick.css" >
+    <link rel="stylesheet" type="text/css" href="/css/slick.css">
     <link rel="stylesheet" type="text/css" href="/css/animate.css">
     <link rel="stylesheet" type="text/css" href="/css/style.css">
     <link rel="stylesheet" type="text/css" href="/css/responsive.css">
@@ -18,31 +19,35 @@
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 
     <link rel="icon" href="{{ url('images/logo.svg') }}">
-    <title>{{$setting->site_name}}</title>
-
-    <meta name="keywords" content="{{$setting->site_keyword}}" />
-	<meta property="og:url" content="<?= request()->url()  ?>" />
-	<meta property="og:type" content="{{$config->site_name}}" />
-	<meta property="og:title" content="{{$config->site_name}}" />
-	<meta property="og:description" content="{{$site_description}}" />
-	<meta property="og:image" content="{{ url('images/logo.svg') }}" />
 
 
-	<link rel="canonical" href="<?= request()->url()  ?>" />
 
-	<meta name="twitter:card" content="{{$setting->site_name}}" />
-	<meta name="twitter:title" content="{{$setting->site_name}}" />
-	<meta name="copyright" content="{{$setting->site_name}}" />
-	<meta name="author" content="{{$setting->site_name}}" />
-	<meta name="GENERATOR" content="{{$setting->site_name}}" />
-	<meta name="twitter:description" content="{{$site_description}}" />
-	<meta name="DC.description" content="{{$site_description}}" />
-	<meta name="twitter:site" publiccontent="{{ request()->fullUrl() }}" />
-	<meta name="DC.language" scheme="ISO639-1" content="en" />
-	<meta name="DC.identifier" content="{{ request()->fullUrl() }}" />
-	<meta name="DC.subject" content="{{$setting->site_keyword}}" />
-	<meta name="twitter:image" content="{{ url('images/logo.svg') }}" />
+    <title>{{ $settings['site_name'] }}</title>
 
+    <meta name="keywords" content="{{ $settings['site_keyword'] }}" />
+    <meta property="og:url" content="<?= request()->url() ?>" />
+    <meta property="og:type" content="{{ $settings['site_name'] }}" />
+    <meta property="og:title" content="{{ $settings['site_name'] }}" />
+    <meta property="og:description" content="{{ $settings['site_description'] }}" />
+    <meta property="og:image" content="{{ url('images/logo.svg') }}" />
+
+
+    <link rel="canonical" href="<?= request()->url() ?>" />
+
+    <meta name="twitter:card" content="{{ $settings['site_name'] }}" />
+    <meta name="twitter:title" content="{{ $settings['site_name'] }}" />
+    <meta name="copyright" content="{{ $settings['site_name'] }}" />
+    <meta name="author" content="{{ $settings['site_name'] }}" />
+    <meta name="GENERATOR" content="{{ $settings['site_name'] }}" />
+    <meta name="twitter:description" content="{{ $settings['site_description'] }}" />
+    <meta name="DC.description" content="{{ $settings['site_description'] }}" />
+    <meta name="twitter:site" publiccontent="{{ request()->fullUrl() }}" />
+    <meta name="DC.language" scheme="ISO639-1" content="en" />
+    <meta name="DC.identifier" content="{{ request()->fullUrl() }}" />
+    <meta name="DC.subject" content="{{ $settings['site_keyword'] }}" />
+    <meta name="twitter:image" content="{{ url('images/logo.svg') }}" />
+
+    @yield('metadata')
 <body>
 
     <div class="header i-item top-show">
@@ -66,10 +71,12 @@
         </div>
     </div>
 
-@yield('content')
+    @yield('content')
 
     <div id="fb-root"></div>
-    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v13.0&appId=2240291739529093&autoLogAppEvents=1" nonce="mLFkBIqK"></script>
+    <script async defer crossorigin="anonymous"
+        src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v13.0&appId=2240291739529093&autoLogAppEvents=1"
+        nonce="mLFkBIqK"></script>
     <script src="/js/jquery.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="/js/locomotive-scroll.js" type="text/javascript"></script>
@@ -77,4 +84,5 @@
     <script src="/js/slick.js"></script>
     <script src="/js/main.js"></script>
 </body>
+
 </html>
