5ad905240a506663b4e5a0250b601e9d5877da23
Thiên Nguyễn <broagii@yahoo.com>
2023-12-26 19:38:35 +0700

    get primay video

--- app/Http/Controllers/Video/VideoController.php
+++ app/Http/Controllers/Video/VideoController.php
@@ -14,12 +14,13 @@ class VideoController extends Controller
         return view('video.index')->with('related_videos', $related_videos);
       }
 
-      $video = Video::where('slug', $slug)->first();
-      if (!$video) {
-        abort(404);
-      }
+      $video = Video::where('primary', 1)->first();
+      // if (!$video) {
+      //   abort(404);
+      // }
 
-      $related_videos = Video::where('id', '!=', $video->id)->orderBy("ordering")->get();
+      // $related_videos = Video::where('id', '!=', $video->id)->orderBy("ordering")->get();
+      $related_videos = Video::orderBy("ordering")->get();
 
       return view('video.index', ["video" => $video])->with('related_videos', $related_videos);
     }
