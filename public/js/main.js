d026dd9a1157e58de8c72220fecdd57f32ae8073
Cao Nguyen <nguyen03091989@gmail.com>
2024-01-21 13:32:00 +0700

    Fix loading and members

--- public/js/main.js
+++ public/js/main.js
@@ -82,6 +82,26 @@ $(document).ready(function () {
         ],
     });
 
+    $(".slider-members").slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: true,
+        dots: false,
+        infinite: false,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    dots: true,
+                },
+            },
+        ],
+    });
+
     $(".toogle-a").click(function () {
         $(".menu").toggleClass("show");
         $(".bg-menu").toggleClass("active");
@@ -285,6 +305,10 @@ $(document).ready(function () {
         $(".popup-album-photo .slider-for").slick("setPosition");
         $(".popup-album-photo .slider-nav").slick("setPosition");
     });
+
+    setTimeout(function(){ 
+        $('.loading').addClass('hide')
+    }, 3000);
 
 });
 
